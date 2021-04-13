383fa76ad0b3ed76516a5346ea6e4d76ed03d7b9
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2021-04-13 06:48:27 +0200

    interface testing

--- src/main/java/UrlMain.java
+++ src/main/java/UrlMain.java
@@ -4,5 +4,11 @@ public class UrlMain {
         String param1 = url.getParam("param1", "url_example?param1=99&param2=string");
         System.out.println(param1);
 
+
+        Url url1 = new ExtendedUrl();
+        String param2 = url1.getParam("param2", "url_example/param2.55/param3.byte");
+        String param3 = url1.getParam("param3", "url_example/param2.55/param3.byte");
+
+        System.out.println(param2 + param3);
     }
 }
